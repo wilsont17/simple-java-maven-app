b2ed7e4e3ef41dc1eab0b7a00b514b17f5e185a3
Tyler Wilson <wilsont17@nku.edu>
2018-03-18 18:41:51 -0400

    h

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env /bin/sh
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
-set -x
 ./gradlew install
-set +x
 
-
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
 ./gradlew run
