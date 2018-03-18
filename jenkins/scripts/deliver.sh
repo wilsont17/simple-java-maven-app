1fc4aad571b0887fbb69988d73b3b9bd40196b7a
Tyler Wilson <wilsont17@nku.edu>
2018-03-18 18:37:11 -0400

    hg

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,20 +5,9 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-gradle publishToMavenLocal
+./gradlew install
 set +x
 
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-set -x
-NAME='my-app'
-set +x
-
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION='1.0-SNAPSHOT'
-set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
